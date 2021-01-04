4763edcd2aca5d3c4047c7a8469b1581abf4a66a
boranfrkn <52128580+boranfrkn@users.noreply.github.com>
2021-01-04 17:16:40 +0300

    unfair computer

---
+++ rock_paper_scissors.py
@@ -0,0 +1,26 @@
+class Computer:
+    def __init__(self):
+        self.get_choice = self.get_choice()
+    def get_choice(self):
+        human = Human
+        human_choice = human.set_choice(human)
+        #kağıdı makas, makası taş, taşı kağıt
+        if human_choice == "paper":
+            computer_choice = "scissors"
+            print(f"Sorry, but the computer chose {computer_choice}")
+        elif human_choice == "rock":
+            computer_choice = "paper"
+            print(f"Sorry, but the computer chose {computer_choice}")
+        elif human_choice == "scissors":
+            computer_choice = "rock"
+            print(f"Sorry, but the computer chose {computer_choice}")
+            
+class Human:
+    def __init__(self):
+        pass
+    def set_choice(self):
+        self.set_choice = input().lower()
+        return self.set_choice
+        
+if __name__ == '__main__':
+    cumputer = Computer()

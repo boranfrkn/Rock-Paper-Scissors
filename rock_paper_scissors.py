fb3544c842040790f9a8325e5596298e32d8879a
boranfrkn <52128580+boranfrkn@users.noreply.github.com>
2021-01-08 02:02:08 +0300

    Update rock_paper_scissors.py
    fair game

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,18 +1,35 @@
+import random
 class Computer:
     def __init__(self):
         self.get_choice = self.get_choice()
     def get_choice(self):
+        random_choice = random.randint(1, 3)
         human = Human
         human_choice = human.set_choice(human)
-        if human_choice == "paper":
-            computer_choice = "scissors"
-            print(f"Sorry, but the computer chose {computer_choice}")
-        elif human_choice == "rock":
+        if random_choice == 1:
             computer_choice = "paper"
-            print(f"Sorry, but the computer chose {computer_choice}")
-        elif human_choice == "scissors":
+            if human_choice == "scissors":
+                print(f"Well done. The computer chose {computer_choice} and failed")
+            elif human_choice == "paper":
+                print(f"There is a draw ({computer_choice})")
+            else:
+                print(f"Sorry, but the computer chose {computer_choice}")
+        elif random_choice == 2:
             computer_choice = "rock"
-            print(f"Sorry, but the computer chose {computer_choice}")
+            if human_choice == "paper":
+                print(f"Well done. The computer chose {computer_choice} and failed")
+            elif human_choice == "rock":
+                print(f"There is a draw ({computer_choice})")
+            else:
+                print(f"Sorry, but the computer chose {computer_choice}")
+        else:
+            computer_choice = "scissors"
+            if human_choice == "rock":
+                print(f"Well done. The computer chose {computer_choice} and failed")
+            elif human_choice == "scissors":
+                print(f"There is a draw ({computer_choice})")
+            else:
+                print(f"Sorry, but the computer chose {computer_choice}")
 class Human:
     def __init__(self):
         pass
